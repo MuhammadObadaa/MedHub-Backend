613ff436c912457b2a1a0124db964a92058c193b
Muhammad Obadaa Almasri <obadaa.masri@gmail.com>
2023-12-07 22:03:30 +0300

    add insertion automation script

--- database/customAutomations/DBinsertion.ps1
+++ database/customAutomations/DBinsertion.ps1
@@ -1,21 +1,16 @@
 Write-Host "Welcome to MedHub DataBase insertion automation v1.0.0"  -ForegroundColor DarkYellow
 $choice = 0
 $imageInputMessage = "input medicine image (just its name and extension like 'image.png' _make sure it's in /app directory in laravel project_ or just leave it empty)"
-$null = ""
-while($choice -eq 0)
-{
-    while($choice -lt 1 -or $choice -gt 3)
-    {
+while ($choice -eq 0) {
+    while ($choice -lt 1 -or $choice -gt 3) {
         Write-Host "Choose (by number) model to insert :" -ForegroundColor DarkYellow
         Write-Host "1)medicine   2)category   3)exit " -ForegroundColor DarkCyan
         $choice = Read-Host
     }
-    if($choice -eq 3)
-    {
+    if ($choice -eq 3) {
         break
     }
-    elseif($choice -eq 1)
-    {
+    elseif ($choice -eq 1) {
         $name = Read-Host -Prompt 'input medicine name '
         $ar_name = Read-Host -Prompt 'input medicine ar_name '
         $scientificName = Read-Host -Prompt 'input medicine scientificName '
@@ -28,8 +23,8 @@ while($choice -eq 0)
         $price = Read-Host -Prompt 'input medicine price '
         $expirationDate = Read-Host -Prompt 'input medicine expiration Date (yyyy-MM-dd)'
         $image = Read-Host -Prompt $imageInputMessage
-        if($image -ne $null)
-        {
+        Write-Host $image
+        if ($image) {
             $image = 'app/' + $image
         }
         $created_at = Get-Date -Format 'yyyy-M-dd'
@@ -37,8 +32,7 @@ while($choice -eq 0)
         VALUES ('$name','$ar_name','$scientificName','$ar_scientificName','$category_id','$brand','$description','$ar_description','$quantity','$expirationDate','$price','$image','$created_at','$created_at')"
         mysql -u root -e $query
     }
-    else
-    {
+    else {
         $name = Read-Host -Prompt 'input category name '
         $ar_name = Read-Host -Prompt 'input category ar_name '
         $created_at = Get-Date -Format 'yyyy-M-dd'
@@ -46,12 +40,10 @@ while($choice -eq 0)
         VALUES ('$name','$ar_name','$created_at','$created_at')"
         mysql -u root -e $query
     }
-    if($?)
-    {
+    if ($?) {#checks the last command if it was executed successfully
         Write-Host "Recorded!" -ForegroundColor DarkGreen
     }
-    else
-    {
+    else {
         Write-Host "Something went wrong!" -ForegroundColor DarkRed
     }
     $choice = 0
@@ -59,4 +51,5 @@ while($choice -eq 0)
 
 Write-Host "Thank you for using my automation .. go and check what the hack you've just made with your DB :)" -ForegroundColor DarkGreen
 
-#Copyright Muhammad Obadaa Almasri 2023
+
+# Muhammad Obadaa Almasri 2023
