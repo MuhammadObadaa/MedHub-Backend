b5e5b9118308361a1f267e027bfe7a6e105f1d90
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-30 17:52:58 +0300

    add pdf package

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'http://127.0.0.1:8000/*',
-        'http://f0fe-37-19-205-195.ngrok-free.app/*'
+        'http://f0fe-37-19-205-195.ngrok-free.app/*',
+        'http://192.168.43.98:8000/*',
+        'http://192.168.42.2:8000/*',
     ];
 }
