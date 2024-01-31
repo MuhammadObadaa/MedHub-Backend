a9f53741af15afe252ba6311807991231484a2dd
Muhammad Obadaa Almasri <obadaa.masri@gmail.com>
2024-01-31 22:06:47 +0300

    merging first file of migration

--- database/migrations/2023_11_23_195839_create_medicines_table.php
+++ database/migrations/2023_11_23_195839_create_medicines_table.php
@@ -14,10 +14,13 @@ return new class extends Migration
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('ar_name');
             $table->string('scientificName');
+            $table->string('ar_scientificName');
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('brand');
             $table->text('description');
+            $table->text('ar_description');
             $table->unsignedInteger('quantity')->default(0);
             $table->date('expirationDate');
             $table->unsignedInteger('price');
